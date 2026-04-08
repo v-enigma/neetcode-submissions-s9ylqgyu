c12dd764e1d092edbc215d3b3f12b8ffc8066871
Venugopal <120632814+v-enigma@users.noreply.github.com>
2026-04-08 16:47:31 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        letterMap = {}
+        for i in s1:
+            if i in letterMap.keys():
+                letterMap[i] = letterMap[i]+1
+            else:
+                letterMap[i] = 1
+        left = 0
+        right = len(s2)
+        intial = -1
+        while left < right :
+            if intial ==-1 and  s2[left] in letterMap.keys():
+                intial = left
+            elif intial >= 0 and s2[left] not in letterMap.keys():
+                length = left-intial 
+                if length != len(s1):
+                    intial = left
+                else:
+                    return True
+            left = left + 1
+        return False
+
+                
+        
+        
