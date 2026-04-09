5a07edd403b3ba5314cd368f2fbf63b6f34feb21
Venugopal <120632814+v-enigma@users.noreply.github.com>
2026-04-09 22:42:56 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,31 @@
+class MinStack:
+
+    def __init__(self):
+        self.storage = []
+        self.min = None
+        
+
+    def push(self, val: int) -> None:
+        self.storage.insert(0,val)
+        if not self.min:
+            self.min = val
+        elif self.min > val:
+            self.min = val
+        
+
+    def pop(self) -> None:
+        removed = self.storage.pop(0)
+        if removed == self.min:
+            self.min = self.storage[0]
+            for i in range(1,len(self.storage)):
+                if self.min > self.storage[i]:
+                    self.min = self.storage[i]
+
+    def top(self) -> int:
+        return self.storage[0]
+        
+
+    def getMin(self) -> int:
+        return self.min
+
+        
