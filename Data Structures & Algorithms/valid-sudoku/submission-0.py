6699a32b25cbd28bb02d057795cf66655ec24fdb
Venugopal <120632814+v-enigma@users.noreply.github.com>
2026-04-17 23:49:27 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        gridMap = {}
+        rowMap = {}
+        columnMap = {}
+        for i in range(9):
+            for j in range(9):
+                if board[i][j]!= ".":
+                    if rowMap.get(i) :
+                        if board[i][j] in rowMap.get(i) :
+                            return False
+                        else:
+                            rowMap.get(i).add(board[i][j])
+                    else:
+                        rowMap[i] = set()
+                        rowMap[i].add(board[i][j])
+                    if columnMap.get(i):
+                        if board[i][j] in columnMap.get(i) :
+                            return False
+                        else:
+                            columnMap.get(i).add(board[i][j])
+                    else:
+                        columnMap[i] = set()
+                        columnMap[i].add(board[i][j])
+                    if gridMap.get((i//3, j//3)):
+                        if board[i][j] in gridMap.get((i//3,j//3)):
+                            return False
+                        else:
+                            gridMap.get((i//3, j//3)).add(board[i][j])
+                    else:
+                        gridMap[(i//3,j//3)] = set()
+                        gridMap[(i//3,j//3)].add(board[i][j])
+        return True
+
+        
